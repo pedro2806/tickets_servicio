6802110463f2675a23711dc64ba5be1095c0f381
IamDef4lt <amraml8a@gmail.com>
2025-03-26 12:26:15 -0600

    cambiando titulo de nuevo

--- inicio.php
+++ inicio.php
@@ -15,7 +15,7 @@
     <meta name = "description" content = "">
     <meta name = "author" content = "">
 
-    <title>MESS TICKETS DE SERVICIO</title>
+    <title>MESS - TICKETS DE SERVICIO</title>
 
     <!-- Custom fonts for this template-->
     <link href = "vendor/fontawesome-free/css/all.min.css" rel = "stylesheet" type = "text/css">
